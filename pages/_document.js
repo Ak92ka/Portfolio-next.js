19e0b43b68fccc62c7f59e257c6aba4f64fbfe9d
Ahmed <ahmedalkhalaf@outlook.fr>
2024-11-02 21:27:07 +0100

    ""

--- pages/_document.js
+++ pages/_document.js
@@ -6,13 +6,11 @@ class MyDocument extends Document {
     return (
       <Html lang="fr">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="icon" type="image/svg+xml" href="/logo.svg" />
+          <link rel="icon" href="https://raw.githubusercontent.com/Ak92ka/Portfolio-next.js/refs/heads/main/public/favicon.ico" />
           <meta
             name="description"
             content="Découvrez le portfolio de Ahmed Khalaf, développeur web, incluant ses expériences, projets, et compétences en développement web moderne et créatif." />
           <link rel="apple-touch-icon" href="/logo192.png" />
-          <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
           <noscript>You need to enable JavaScript to run this app.</noscript>
