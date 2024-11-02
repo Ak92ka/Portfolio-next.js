55edc25c1d968755715d9e31a5dd8900872e7a54
Ahmed <ahmedalkhalaf@outlook.fr>
2024-11-02 21:49:23 +0100

    Revert "contact css"
    This reverts commit 0f850e90861c7f127fc0cd95d4f356657e289909.

--- components/Banner.js
+++ components/Banner.js
@@ -2,11 +2,11 @@
 export default function Banner() {
     return(
         <div className="banner">
-            <img src="/Assets/bonjour.webp" 
+            <img src="https://raw.githubusercontent.com/Ak92ka/Portfolio-next.js/refs/heads/main/public/Assets/bonjour.webp" 
                         className="banner-gif"
                         alt="Joli soleil avec des lunettes disant bonjour"
                         width="480"
-                        srcSet="/Assets/bonjour.webp 480w, /Assets/bonjour.webp 800w"
+                        srcSet="https://raw.githubusercontent.com/Ak92ka/Portfolio-next.js/refs/heads/main/public/Assets/bonjour.webp 480w, https://raw.githubusercontent.com/Ak92ka/Portfolio-next.js/refs/heads/main/public/Assets/bonjour.webp 800w"
                         sizes="(max-width: 480px) 100vw, 480px" />
             <div className="banner-text">
             <h1>Ahmed Khalaf</h1>
