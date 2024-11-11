6a1118ce9af487ce7c87f642a78b05ae5ded61c5
Ahmed <ahmedalkhalaf@outlook.fr>
2024-11-11 17:18:42 +0100

    status bar color

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,3 @@
-// pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
@@ -7,6 +6,9 @@ class MyDocument extends Document {
       <Html lang="fr">
         <Head>
           <link rel="apple-touch-icon" href="/logo192.png" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          <meta name="theme-color" content="rgba(249, 249, 249, 1)" />
+          
         </Head>
         <body>
           <noscript>You need to enable JavaScript to run this app.</noscript>
